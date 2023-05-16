22f05d062adec08e32609255dac8568c388c1aa5
Liz Goodin <elizabethagoodin@gmail.com>
2023-05-16 14:23:53 +1200

    changed index to php

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Liz Goodin | Resume</title>
+	
+  <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/respond.js@1.4.2/dest/respond.min.js"></script>
+    <![endif]-->
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	</head>
+
+	<body>
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Liz Goodin</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+          </ul>
+          
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="personal_statement.html">Personal Statement</a></li>
+            
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container -->
+    </nav>
+
+    <div class="container">
+    <!-- Top Resume Info -->
+      <div class="panel panel-default">
+          <div class="panel-heading">
+          <h3 class="panel-title"><strong>Super Duper Amazing person</strong></h3>
+          </div>
+          <div class="panel-body">
+
+            <div class="col-md-8">
+              <strong>Job at the place</strong><br/>
+              <p align="justify">I went and got born! in the place i did the thing before doing another thing and all of the things were amazing. Some notable highlights for me was the jellyfish, the eagle train, the caffeination of dressed up stone statues, and a mothers smile.<br/><br/></p>
+            </div>
+            <div class="col-md-4"align="right">
+              <em>1991-current</em>
+            </div>
+
+            <div class="col-md-8">
+              <strong>Job at 2nd placee</strong><br/>
+              <p align="justify">I I worked and worked and worked and worked and worked and worked and worked and worked and worked and worked and worked and worked and worked and worked and worked and worked and worked and worked and worked <br/><br/></p>
+            </div>
+            <div class="col-md-4"align="right">
+              <em>2001-2002</em>
+            </div>
+
+            <div class="col-md-8">
+              <strong>Job at third place</strong><br/>
+              <p align="justify">snoopledoople applesauce friends and worked and worked and workedand worked and worked and workedand worked and worked and workedand worked and worked and workedand worked and worked and workedand worked and worked and workedand worked and worked and workedand worked and worked and worked Some notable highlights for me was the jellyfish, the eagle train, the caffeination of dressed up stone statues, and a mothers smile.<br/><br/></p>
+            </div>
+            <div class="col-md-4"align="right">
+              <em>2013-2018</em>
+            </div>
+
+          </div>
+        </div>
+    <br/><br/>
+
+
+    <!-- Technical Skills Section -->
+          <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title"><strong>Technical Skills</strong></h3>
+        </div>
+        <div class="panel-body">
+          <div class="col-md-3">Stuff</div>
+          <div class="col-md-3">Stuff</div>
+          <div class="col-md-3">Stuff</div>
+          <div class="col-md-3">Stuff</div>
+        </div>
+      </div>
+      <br/><br/>
+
+    <!-- Education Section -->
+          <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title"><strong>Education</strong></h3>
+        </div>
+        <div class="panel-body">
+          <div class="col-md-1"><img src="/Users/gooli91p/Desktop/HTMLcode/images/zuchini.jpeg" height="45"></div>
+          <div class="col-md-4"><strong>The Education House of Awesomeness</strong><br/>Diploma in smiling<br/>
+            PostGraduate Diploma in laughing<br/><br/>
+            <em>Most Like to try the hardest</em><br/><br/>
+          </div>
+        </div>
+      </div>
+
+
+
+	<br/><br/>
+	<p align="center">&copy; Liz Goodin - All Rights Reserved</p>
+  <br/>
+
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+	</body>
+</html>
